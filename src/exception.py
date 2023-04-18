ae921e26c11c725a24e2421ee5ec7c8b7d24dbc3
Sagar Kandpal <sagariit.kanpur1@gmail.com>
2023-04-18 13:14:42 +0530

    EDA and model training notebook uploaded

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
 import logging
-import logger
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -13,6 +13,7 @@ def error_message_detail(error, error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
+    #It calls the parent constructor (super().__init__()) to initialize the base exception class with the error_message.
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
