2310d72f2415113305d9dbf18bf8f614ea78addd
Sagar Kandpal <sagariit.kanpur1@gmail.com>
2023-04-25 12:28:34 +0530

    Model Trainer file updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 #This DataIngestionConfig decorator class provides any inputs and paths required for data ingestion component
 @dataclass
 class DataIngestionConfig:
@@ -47,3 +50,6 @@ if __name__ == '__main__':
 
     data_transformation = DataTransformation()
     train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_training(train_arr, test_arr))
